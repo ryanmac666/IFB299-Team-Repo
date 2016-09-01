b7da58eeb7c63330abfe85046dfb08811c5a81f5
scott marshall <scott.h.marshall1997@gmail.com>
2016-09-02 09:12:49 +1000

    added landing page

--- CommunityOrganization/CommunityOrganization/urls.py
+++ CommunityOrganization/CommunityOrganization/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('events.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^events/', include('events.urls')),
     url(r'^users/', include('users.urls')),
