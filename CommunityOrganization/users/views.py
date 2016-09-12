0bfbd492407eec42a90bb3878fb180390cedfee8
Yee Chen Fong <yeechen.fong@connect.qut.edu.au>
2016-09-12 16:41:16 +1000

    Register User redirect fix

--- CommunityOrganization/users/views.py
+++ CommunityOrganization/users/views.py
@@ -15,84 +15,93 @@ Display users attending and volunterring events
 TODO: Display user information
 """
 def user_view(request):
+    if not request.user.is_authenticated:
+        return redirect('/users/login')
 
-	if not request.user.is_authenticated:
-		return redirect('/users/login')
+    else:
+        # get all events the user is attending or volunteering
+        data = UserData.objects.get(user=request.user)
 
-	else:
-		#get all events the user is atteneding or volunteering
-		data = UserData.objects.get(user=request.user)
+        attending_list = Event.objects.filter(userattending__user=data)
+        attending_donation_list = event_donation_list(attending_list)
+        attending_data = zip(attending_list, attending_donation_list)
 
-		attending_list = Event.objects.filter(userattending__user=data)
-		attending_donation_list = event_donation_list(attending_list)
-		attending_data = zip(attending_list, attending_donation_list)
+        volunteering_list = Event.objects.filter(userdata__user=request.user)
+        volunteering_donation_list = event_donation_list(volunteering_list)
+        volunteering_data = zip(volunteering_list, volunteering_donation_list)
 
-		volunteering_list = Event.objects.filter(userdata__user=request.user)
-		volunteering_donation_list = event_donation_list(volunteering_list)
-		volunteering_data = zip(volunteering_list, volunteering_donation_list)
+        context = {
+            'attending_data': attending_data,
+            'volunteering_data': volunteering_data,
+            'user': request.user,
+        }
 
-		context = {
-		    'attending_data': attending_data,
-		    'volunteering_data': volunteering_data,
-		    'user': request.user,
-		}
+        return render(request, 'users/users.html', context)
 
-		return render(request, 'users/users.html', context)
 
 """
 Allow users to create an account
 TODO: Ask for information such as first and last name
 """
 def user_signup_view(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
 
-	if request.method == 'POST':
-		form = UserCreateForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            send_mail(
+                'Registration successful',
+                'Thanks for registering',
+                'ifb299dummyemail@gmail.com',
+                ['ifb299dummyemail@gmail.com'],
+                fail_silently=False,
+            )
 
-		if form.is_valid():
-			form.save()
-			send_mail(
-                            'Registration successful',
-                            'Thanks for registering',
-                            'ifb299dummyemail@gmail.com',
-                            ['ifb299dummyemail@gmail.com'],
-                            fail_silently=False,
-                        )
-			return user(request)
+            if user is not None:
+                login(request, user)
+                return redirect('/events')
+            else:
+                return redirect('/users/login')
 
-	form = UserCreateForm()
-	context = {
-	    'form': form,
-	    'user': request.user,
-	}
+            # return user(request)
 
-	return render(request, 'users/signup.html', context)
+    form = UserCreateForm()
+    context = {
+        'form': form,
+        'user': request.user,
+    }
+
+    return render(request, 'users/signup.html', context)
+
 
 """
 Allow users to login
 """
 def user_login_view(request):
-	context = {
-	    'user': request.user,
-	}
+    context = {
+        'user': request.user,
+    }
 
-	return render(request, 'users/login.html', context)
+    return render(request, 'users/login.html', context)
+
 
 """
 Authenticate user login data for POST request
 """
 def user_auth_view(request):
-	username = request.POST.get('username', '')
-	password = request.POST.get('password', '')
-	user = authenticate(username=username, password=password)
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = authenticate(username=username, password=password)
 
-	if user is not None:
-		login(request, user)
+    if user is not None:
+        login(request, user)
 
-		return redirect('/events')
+        return redirect('/events')
 
-	else:
+    else:
 
-		return redirect('/users/login')
+        return redirect('/users/login')
+
 
 """
 Log users out
