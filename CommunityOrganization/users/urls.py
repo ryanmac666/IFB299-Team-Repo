5d57851f47b983379902f38876f23bd358efc94f
scott marshall <scott.h.marshall1997@gmail.com>
2016-09-21 18:50:16 +1000

    Removed old auth url
    Fixed error caused by keeping the old auth url.

--- CommunityOrganization/users/urls.py
+++ CommunityOrganization/users/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
             url(r'^signup', views.user_signup_view, name='user_signup_view'),
             url(r'^logout', views.user_logout_view, name='user_logout_view'),
             url(r'^login', views.user_login_view, name='user_login_view'),
-            url(r'^auth', views.user_auth_view, name='user_auth_view'),
             ]
