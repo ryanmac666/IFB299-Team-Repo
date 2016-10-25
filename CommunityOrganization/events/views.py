ffa2c5228646e28153bb00788571072f02e0f22a
scott marshall <scott.h.marshall1997@gmail.com>
2016-10-25 22:44:15 +1000

    added Withdraw Attendance and Withdraw Volunteering
    Users can not revoke their offer to volunteer and attend

--- CommunityOrganization/events/views.py
+++ CommunityOrganization/events/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db.models import Q
 from .models import Event
-from .forms import EventCreateForm
 from users.models import UserData, UserDonation, UserAttending
 from .utils import event_user_donation_list, event_user_donation_total, event_donation_total, event_donation_list, event_notify, event_user_family, event_attendee_donation_list, donation_total
 from django.contrib import messages
@@ -89,7 +88,7 @@ def event_view(request, event_id):
             #test if user is attending or volunteering for the event
             data = UserData.objects.get(user=request.user)
             is_attending = Event.objects.filter(userattending__user=data, id=event_id).exists()
-            is_volunteering = Event.objects.filter(userattending__user=data, id=event_id).exists()
+            is_volunteering = UserData.objects.filter(user=request.user, events_volunteering__id=event_id).exists()
             user_donations = donation_total(data)
             #get notifications
             notify_list = request.user.notifications.unread()[:5]
@@ -116,7 +115,7 @@ def event_view(request, event_id):
 """
 Add user to event attendee table
 """
-def event_attend_view(request, event_id):
+def event_attend_view(request, event_id, option):
     amount = request.POST.get('amount', 0)
 
     if not request.user.is_authenticated:
@@ -126,24 +125,25 @@ def event_attend_view(request, event_id):
         try:
             event = Event.objects.get(id=event_id)
             user = UserData.objects.get(user=request.user)
-            #make sure user is not already attending this event
-            if UserAttending.objects.filter(user=user, event=event).exists():
-                raise Http404("Already in event! p.s this is for debuging only. make a page!")
 
-            else:
+            if option == "1": #create
+                #ensure user is not already attending
+                if UserAttending.objects.filter(user=user, event=event).exists():
+                    return redirect('/events/' + event_id)
+
                 attendingUser = UserAttending(user=user, event=event, family=amount)
                 attendingUser.save()
                 event_notify(request, event, "You are attending", "is attending")
 
+            
+            if option == "2": #delete
+                attendingUser = UserAttending.objects.get(user=user, event=event)
+                attendingUser.delete()
+
         except Event.DoesNotExist:
             raise Http404("Event does not exist")
 
-        except UserData.DoesNotExist:
-            raise Http404("Missing user data! what?!")
-
-    event_list = Event.objects.order_by('-start_date')[:5]
-
-    return redirect('/events/')
+    return redirect('/events/' + event_id)
 
 """
 Add a donation to the event
@@ -176,29 +176,35 @@ def event_donate_view(request, event_id):
 """
 Add user to volunteer table
 """
-def event_volunteer_view(request, event_id):
+def event_volunteer_view(request, event_id, option):
     if not request.user.is_authenticated:
         return redirect('/users/login')
 
     else:
         try:
             event = Event.objects.get(id=event_id)
-            #make sure user is not already volunteer for the event this event
-            if Event.objects.filter(id=event_id, userdata__user=request.user).exists():
-                raise Http404("already volunteering! p.s this is for debuging only. make a page!")
+            
+            if option == "1": #create
+                #make sure user is not already volunteer for the event this event
+                if Event.objects.filter(id=event_id, userdata__user=request.user).exists():
+                    return redirect('/events/' + event_id)
 
-            else:
                 user_voluntee = UserData.objects.get(user=request.user)
                 user_voluntee.events_volunteering.add(event)
                 user_voluntee.save()
                 event_notify(request, event, "You are volunteering for", "is volunteering for")
+
+            if option == "2": #delete
+                user_voluntee = UserData.objects.get(user=request.user)
+                user_voluntee.events_volunteering.remove(event)
+                user_voluntee.save()
 
         except Event.DoesNotExist:
             raise Http404("Event does not exist")
 
     #event_list = Event.objects.order_by('-start_date')[:5]
     
-    return redirect('/events/')
+    return redirect('/events/' + event_id)
 
 """
 Display a list of all volunteers
